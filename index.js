7302dc65a1884a3325e101b108f7f222d338514d
arthurtanjaya <51280002+arthurtanjaya@users.noreply.github.com>
2019-08-13 21:25:00 +0700

    Added keyboard press

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ var clicks = 0;
 document.getElementById("clickme").addEventListener("click",
     function () {
         ++clicks;
-        document.getElementById("counter").innerHTML = clicks;
+        document.getElementById("counter").textContent = clicks;
+    },
+    false);
+
+document.addEventListener("keydown",
+    function (e) {
+        document.getElementById("lastpressed").textContent = e.code;
     },
     false);
